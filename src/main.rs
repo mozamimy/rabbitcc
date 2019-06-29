db62d7d330514d3c2b0badefe87d0f48babcc242
mozamimy <alice@mozami.me>
2019-06-29 13:56:19 +0900

    Enable to handle expression like 5+20-4
    https://www.sigbus.info/compilerbook#%E3%82%B9%E3%83%86%E3%83%83%E3%83%972%E5%8A%A0%E6%B8%9B%E7%AE%97%E3%81%AE%E3%81%A7%E3%81%8D%E3%82%8B%E3%82%B3%E3%83%B3%E3%83%91%E3%82%A4%E3%83%A9%E3%81%AE%E4%BD%9C%E6%88%90

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+enum Operator {
+    Mov,
+    Add,
+    Sub,
+}
+
+impl Operator {
+    fn opcode(&self) -> &str {
+        match self {
+            Operator::Add => "add",
+            Operator::Sub => "sub",
+            Operator::Mov => "mov",
+        }
+    }
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
@@ -9,6 +25,38 @@ fn main() {
     println!(".intel_syntax noprefix");
     println!(".global main");
     println!("main:");
-    println!("  mov rax, {}", args[1]);
+
+    let mut num_buf = String::with_capacity(8);
+    let mut operator = Operator::Mov;
+    for (i, c) in args[1].chars().enumerate() {
+        if i == 0 && !('0'..='9').contains(&c) {
+            eprintln!("Unexpected character: {}", c);
+            std::process::exit(1);
+        }
+
+        match c {
+            '+' => {
+                println!("  {} rax, {}", operator.opcode(), num_buf);
+                num_buf.clear();
+                operator = Operator::Add;
+            }
+            '-' => {
+                println!("  {} rax, {}", operator.opcode(), num_buf);
+                num_buf.clear();
+                operator = Operator::Sub;
+            }
+            num @ '0'..='9' => {
+                num_buf.push(num);
+            }
+            unexpected @ _ => {
+                eprintln!("Unexpected character: {}", unexpected);
+                std::process::exit(1);
+            }
+        }
+
+        if i == args[1].len() - 1 {
+            println!("  {} rax, {}", operator.opcode(), num_buf);
+        }
+    }
     println!("  ret");
 }
