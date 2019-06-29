61f5707dae922c9aa2e4d51ad97693b6e1f05fb8
mozamimy <alice@mozami.me>
2019-06-29 12:44:41 +0900

    Add a tiny test framework as test.sh
    https://www.sigbus.info/compilerbook#%E3%83%A6%E3%83%8B%E3%83%83%E3%83%88%E3%83%86%E3%82%B9%E3%83%88%E3%81%AE%E4%BD%9C%E6%88%90

---
+++ test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
+try() {
+  expected="$1"
+  input="$2"
+
+  ./target/debug/rabbitcc "$input" > tmp/tmp.s
+  gcc -o bin/tmp tmp/tmp.s
+  ./bin/tmp
+  actual="$?"
+
+  if [[ "$actual" = "$expected" ]]; then
+    echo "$input => $actual"
+  else
+    echo "$input => $expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+try 0 0
+try 42 42
+
+echo OK
