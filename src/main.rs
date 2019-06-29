7274684fbe28a292886412ec64a91dbe04b90e9d
mozamimy <alice@mozami.me>
2019-06-29 12:21:07 +0900

    Implement very very simple first compiler
    https://www.sigbus.info/compilerbook#%E3%82%B3%E3%83%B3%E3%83%91%E3%82%A4%E3%83%A9%E6%9C%AC%E4%BD%93%E3%81%AE%E4%BD%9C%E6%88%90

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Invalid argument length.");
+        std::process::exit(1);
+    }
+
+    println!(".intel_syntax noprefix");
+    println!(".global main");
+    println!("main:");
+    println!("  mov rax, {}", args[1]);
+    println!("  ret");
 }
